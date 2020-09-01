e44e289c05227d0e84e9e6867e2510a4c644c353
Zach Stearns <zach.stearns92@gmail.com>
2020-09-01 10:50:59 -0600

    Add tests for main characters and add show, almost complete actors by show

--- test/network_test.rb
+++ test/network_test.rb
@@ -5,5 +5,57 @@ require './lib/show'
 require './lib/network'
 
 class ShowTest < Minitest::Test
+  def test_it_exists
+    nbc = Network.new("NBC")
+  end
 
+  def test_name
+    nbc = Network.new("NBC")
+    assert_equal "NBC", nbc.name
+  end
+
+  def test_network_starts_with_no_shows
+    nbc = Network.new("NBC")
+    assert_equal [], nbc.shows
+  end
+
+  def test_add_shows
+    nbc = Network.new("NBC")
+    kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
+    michael_knight = Character.new({name: "Michael Knight", actor: "David Hasselhoff", salary: 1_600_000})
+    knight_rider = Show.new("Knight Rider", "Glen Larson", [michael_knight, kitt])
+    leslie_knope = Character.new({name: "Leslie Knope", actor: "Amy Poehler", salary: 2_000_000})
+    ron_swanson = Character.new({name: "Ron Swanson", actor: "Nick Offerman", salary: 1_400_000})
+    parks_and_rec = Show.new("Parks and Recreation", "Michael Shur & Greg Daniels", [leslie_knope, ron_swanson])
+    nbc.add_show(knight_rider)
+    assert_equal [knight_rider], nbc.shows
+    nbc.add_show(parks_and_rec)
+    assert_equal [knight_rider, parks_and_rec], nbc.shows
+  end
+
+  def test_main_character
+    nbc = Network.new("NBC")
+    kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
+    michael_knight = Character.new({name: "Michael Knight", actor: "David Hasselhoff", salary: 1_600_000})
+    knight_rider = Show.new("Knight Rider", "Glen Larson", [michael_knight, kitt])
+    leslie_knope = Character.new({name: "Leslie Knope", actor: "Amy Poehler", salary: 2_000_000})
+    ron_swanson = Character.new({name: "Ron Swanson", actor: "Nick Offerman", salary: 1_400_000})
+    parks_and_rec = Show.new("Parks and Recreation", "Michael Shur & Greg Daniels", [leslie_knope, ron_swanson])
+    nbc.add_show(knight_rider)
+    nbc.add_show(parks_and_rec)
+    assert_equal [kitt], nbc.main_characters
+  end
+
+  def test_actors_by_show
+    nbc = Network.new("NBC")
+    kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
+    michael_knight = Character.new({name: "Michael Knight", actor: "David Hasselhoff", salary: 1_600_000})
+    knight_rider = Show.new("Knight Rider", "Glen Larson", [michael_knight, kitt])
+    leslie_knope = Character.new({name: "Leslie Knope", actor: "Amy Poehler", salary: 2_000_000})
+    ron_swanson = Character.new({name: "Ron Swanson", actor: "Nick Offerman", salary: 1_400_000})
+    parks_and_rec = Show.new("Parks and Recreation", "Michael Shur & Greg Daniels", [leslie_knope, ron_swanson])
+    nbc.add_show(knight_rider)
+    nbc.add_show(parks_and_rec)
+    assert_equal ({knight_rider => [characters], parks_and_rec => [characters]}), nbc.actors_by_show
+  end
 end
